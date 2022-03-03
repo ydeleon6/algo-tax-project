b5750f4221284e7be5ef1a1bdd02ca67121aa0d9
Yahir De Leon <ydeleon6@gmail.com>
2022-03-02 21:04:12 -0600

    Get the master branch working

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import * as indexerApi from "./api/indexer.js";
-import { TransactionAnalyzer, TransactionStagingFileWriter } from "./transactions.js";
+import { TransactionAnalyzer } from "./transactions.js";
 import { TransactionFileWriter } from "./data/file-writer.js";
-import { TransactionsDb } from "./data/database.js";
 
 
 (async function main(args) {
@@ -10,7 +9,6 @@ import { TransactionsDb } from "./data/database.js";
     if (!account) {
         throw new Error("Unknown account.");
     }
-    const transactionsCsvWriter = new TransactionStagingFileWriter(indexerApi);
     const options = {
         afterDate: "2021-01-01T00:00:00Z",
         beforeDate: "2021-12-31T23:59:59Z",
@@ -19,17 +17,18 @@ import { TransactionsDb } from "./data/database.js";
         address: accountAddress
     };
 
-    let currentPage = 0;
     let transactionData = await indexerApi.getTransactionList(options);
     options.nextToken = transactionData['next-token'];
+
+    const transactionFileWriter = new TransactionFileWriter("2021_taxable_events.csv");
+    const transactionAnalyzer = new TransactionAnalyzer(indexerApi, transactionFileWriter);
+    transactionAnalyzer.init();
 
     while (options.nextToken != null) {
         const transactions = transactionData.transactions || [];
 
         transactions.forEach(async (transaction) => {
-            // write all transactions to a .csv so we can cross-check them later.
-            await transactionsCsvWriter.importTransaction(transaction);
-            //await transactionAnalyzer.analyzeTransaction(transaction, accountAddress);
+            await transactionAnalyzer.analyzeTransaction(transaction, accountAddress);
          });
         
         transactionData = await indexerApi.getTransactionList(options);
@@ -40,10 +39,8 @@ import { TransactionsDb } from "./data/database.js";
             transactionData.transactions.length,
             options.nextToken);
         }
-        currentPage++;
     }
 
-    transactionsCsvWriter.close();
-    //fileWriter.close();
-    //fileWriter.writeReport();
+    transactionFileWriter.close();
+    transactionFileWriter.writeReport();
 })(process.argv);
